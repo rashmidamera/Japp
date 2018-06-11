6ce1349e7339817d4e34341fc13d2e974f98153f
user1 <user2@accenture.com>
2018-06-11 12:52:28 +0530

    sop added

--- src/com/App.java
+++ src/com/App.java
@@ -3,5 +3,7 @@ package com;
 public class App {
 public static void main(String[] args) {
 	System.out.println("hello world");
+	System.out.println("hello world Accenture");
+	System.out.println("hello world accenture lkm");
 }
 }
